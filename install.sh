de77a106bfaabe18190a2019bb61da529cad8848
hue <humberto.rodriguez927@gmail.com>
2026-01-19 19:43:15 -0600

    Starship Install and Ghostty terminfo

--- install.sh
+++ install.sh
@@ -9,4 +9,17 @@ if ! command -v chezmoi &> /dev/null; then
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$BIN_DIR"
 fi
 
+if ! command -v starship &> /dev/null; then
+    curl -sS https://starship.rs/install.sh | sh -s -- --yes -b "$BIN_DIR"
+fi
+
 "$BIN_DIR/chezmoi" apply --force --no-tty
+
+GHOSTTY_SOURCE="$HOME/.terminfo/x/xterm-ghostty"
+
+if [ -f "$GHOSTTY_SOURCE" ]; then
+  tic -x "$GHOSTTY_SOURCE"
+else
+  echo "Ghostty terminfo source not found at $GHOSTTY_SOURCE"
+fi
+
