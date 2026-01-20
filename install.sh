155ac7cb9ec3b3f1d9c6ee861fab50e5e215a2f0
hue <humberto.rodriguez927@gmail.com>
2026-01-19 18:58:03 -0600

    Fixed installation path

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ BIN_DIR="$HOME/.local/bin"
 mkdir -p "$BIN_DIR"
 
 if ! command -v chezmoi &> /dev/null; then
-  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b="$BIN_DIR"
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$BIN_DIR"
 fi
 
 "$BIN_DIR/chezmoi" apply --force --tty=false
